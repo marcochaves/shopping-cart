19017a17d50066a14915e12999955480882abc2f
Eric Chaves <erchaves@gmail.com>
2015-06-08 22:38:17 -0400

    using new sample data

--- sampleData.js
+++ sampleData.js
@@ -5,20 +5,36 @@ define(function() {
             name: 'Shop Item A',
             unitPrice: 2,
             bulkPrice: {
-                minItems: 3,
-                unitPrice: 1,
+                minItems: 4,
+                unitPrice: 1.75,
             },
         },
         {
             sku: 'B',
             name: 'Shop Item B',
-            unitPrice: 5,
+            unitPrice: 12,
+        },
+        {
+            sku: 'C',
+            name: 'Shop Item C',
+            unitPrice: 1.25,
+            bulkPrice: {
+                minItems: 6,
+                unitPrice: 1,
+            },
+        },
+        {
+            sku: 'D',
+            name: 'Shop Item D',
+            unitPrice: .15,
         },
     ];
 
     var shopCartItemsPerSku = {
-        A: 1,
-        B: 4,
+        A: 4,
+        B: 2,
+        C: 1,
+        D: 1,
     };
 
     return {
